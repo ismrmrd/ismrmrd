42b208cfa4aa7f60dc07e186498700777cdc9c9b
Michael S. Hansen <michael.hansen@nih.gov>
2012-09-13 13:52:37 -0400

    Small fix to Windows Exports

--- ismrmrd_xsd_export.h
+++ ismrmrd_xsd_export.h
@@ -9,7 +9,7 @@
 #define ISMRMRD_XSD_EXPORT_H_
 
 #if defined (WIN32)
-#if defined (ismrmrd_EXPORTS)
+#if defined (ismrmrd_xsd_EXPORTS)
 #define EXPORTISMRMRDXSD __declspec(dllexport)
 #else
 #define EXPORTISMRMRDXSD __declspec(dllimport)
