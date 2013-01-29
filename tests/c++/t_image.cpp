e9a9c9aa2c57007439c2bd1408420adac0f6ffc4
kvahed <kaveh@vahedipour.de>
2013-01-29 17:30:20 +0100

    Image test

--- tests/c++/t_image.cpp
+++ tests/c++/t_image.cpp
@@ -2,8 +2,6 @@
 #include "ismrmrd_hdf5.h"
 #include "ismrmrd.hxx"
 
-
-
 int main (int args, char** argv) {
 
     using namespace ISMRMRD;
@@ -14,10 +12,17 @@ int main (int args, char** argv) {
     dims.push_back (1);
     dims.push_back (16); // channels
     
-    
     Image<std::complex<float> > img_s (dims);
 
-    return 0;
+    if (!img_s.is_consistent()) // dimensions and data size match?
+        return 1;
+    
+    size_t img_s_gne = img_s.getNumberOfElements();
+    size_t img_s_e   = img_s.elements();
 
+    if (img_s_gne != img_s_e) // header and data match?
+        return 1;
+    
+    return 0;
     
 }
