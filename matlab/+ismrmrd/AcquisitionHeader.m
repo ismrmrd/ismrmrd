6824922e0eaf3535b81a60d53a183de611361ed7
Souheil Inati <souheil.inati@nih.gov>
2013-04-11 14:42:09 -0400

    Removed all of the HDF5 stuff from the matlab interface.  The interface is now just contains what is need for hanling AcquisitionHeader and ImageHeader.  The Java bindings are much better.

--- matlab/+ismrmrd/AcquisitionHeader.m
+++ matlab/+ismrmrd/AcquisitionHeader.m
@@ -18,9 +18,9 @@ classdef AcquisitionHeader
         trajectory_dimensions = uint16(0);             % Indicates the dimensionality of the trajectory vector (0 means no trajectory) %
         sample_time_us = single(0);                    % Time between samples in micro seconds, sampling BW %
         position = zeros(3,1,'single');                % Three-dimensional spatial offsets from isocenter %
-        read_dir = zeros(3,1,'single');         % Directional cosines of the readout/frequency encoding %
-        phase_dir = zeros(3,1,'single');           % Directional cosines of the phase encoding %
-        slice_dir = zeros(3,1,'single');         % Directional cosines of the slice %
+        read_dir = zeros(3,1,'single');                % Directional cosines of the readout/frequency encoding %
+        phase_dir = zeros(3,1,'single');               % Directional cosines of the phase encoding %
+        slice_dir = zeros(3,1,'single');               % Directional cosines of the slice %
         patient_table_position = zeros(3,1, 'single'); % Patient table off-center %
         idx = ismrmrd.EncodingCounters;                % Encoding loop counters, see above %
         user_int = zeros(8,1,'int32');                 % Free user parameters %
@@ -28,42 +28,84 @@ classdef AcquisitionHeader
     end
     
     methods
-       
+        
         % Constructor
-        function obj = AcquisitionHeader(s)
+        function obj = AcquisitionHeader(bytes)
             if (nargin == 1)
-                obj.version = s.version;
-                obj.flags = s.flags;
-                obj.measurement_uid = s.measurement_uid;
-                obj.scan_counter = s.scan_counter;
-                obj.acquisition_time_stamp = s.acquisition_time_stamp;
-                obj.physiology_time_stamp = s.physiology_time_stamp;
-                obj.number_of_samples = s.number_of_samples;
-                obj.available_channels = s.available_channels;
-                obj.active_channels = s.active_channels;
-                obj.channel_mask = s.channel_mask;
-                obj.discard_pre = s.discard_pre;
-                obj.discard_post = s.discard_post;
-                obj.center_sample = s.center_sample;
-                obj.encoding_space_ref = s.encoding_space_ref;
-                obj.trajectory_dimensions = s.trajectory_dimensions;
-                obj.sample_time_us = s.sample_time_us;
-                obj.position = s.position;
-                obj.read_dir = s.read_dir;
-                obj.phase_dir = s.phase_dir;
-                obj_slice_dir = s.slice_dir;
-                obj.patient_table_position = s.patient_table_position;
-                obj.idx = ismrmrd.EncodingCounters(s.idx);
-                obj.user_int = s.user_int;
-                obj.user_float = s.user_float;
-          end
-      end
-
-        % Set methods 
+                off = 1;
+                obj.version                = typecast(bytes(off:off+1),  'uint16'); off=off+2;
+                obj.flags                  = typecast(bytes(off:off+7),  'uint64'); off=off+8;
+                obj.measurement_uid        = typecast(bytes(off:off+3),  'uint32'); off=off+4;
+                obj.scan_counter           = typecast(bytes(off:off+3),  'uint32'); off=off+4;
+                obj.acquisition_time_stamp = typecast(bytes(off:off+3),  'uint32'); off=off+4;
+                obj.physiology_time_stamp  = typecast(bytes(off:off+31), 'uint32'); off=off+32;
+                obj.number_of_samples      = typecast(bytes(off:off+1),  'uint16'); off=off+2;
+                obj.available_channels     = typecast(bytes(off:off+1),  'uint16'); off=off+2;
+                obj.active_channels        = typecast(bytes(off:off+1),  'uint16'); off=off+2;
+                obj.channel_mask           = typecast(bytes(off:off+127),'uint64'); off=off+128;
+                obj.discard_pre            = typecast(bytes(off:off+1),  'uint16'); off=off+2;
+                obj.discard_post           = typecast(bytes(off:off+1),  'uint16'); off=off+2;
+                obj.center_sample          = typecast(bytes(off:off+1),  'uint16'); off=off+2;
+                obj.encoding_space_ref     = typecast(bytes(off:off+1),  'uint16'); off=off+2;
+                obj.trajectory_dimensions  = typecast(bytes(off:off+1),  'uint16'); off=off+2;
+                obj.sample_time_us         = typecast(bytes(off:off+3),  'single'); off=off+4;
+                obj.position               = typecast(bytes(off:off+11), 'single'); off=off+12;
+                obj.read_dir               = typecast(bytes(off:off+11), 'single'); off=off+12;
+                obj.phase_dir              = typecast(bytes(off:off+11), 'single'); off=off+12;
+                obj.slice_dir              = typecast(bytes(off:off+11), 'single'); off=off+12;
+                obj.patient_table_position = typecast(bytes(off:off+11), 'single'); off=off+12;
+                obj.idx              = ismrmrd.EncodingCounters(bytes(off:off+33)); off=off+34;
+                obj.user_int               = typecast(bytes(off:off+31), 'uint32'); off=off+32;
+                obj.user_float             = typecast(bytes(off:off+31), 'single');
+            end
+        end
+        
+        % Convert to Bytes
+        function bytes = toBytes(obj)
+            bytes = zeros(360,1,'int8');
+            off = 1;
+            bytes(off:off+1)   = typecast(obj.version               ,'int8'); off=off+2;
+            bytes(off:off+7)   = typecast(obj.flags                 ,'int8'); off=off+8;
+            bytes(off:off+3)   = typecast(obj.measurement_uid       ,'int8'); off=off+4;
+            bytes(off:off+3)   = typecast(obj.scan_counter          ,'int8'); off=off+4;
+            bytes(off:off+3)   = typecast(obj.acquisition_time_stamp,'int8'); off=off+4;
+            bytes(off:off+31)  = typecast(obj.physiology_time_stamp ,'int8'); off=off+32;
+            bytes(off:off+1)   = typecast(obj.number_of_samples     ,'int8'); off=off+2;
+            bytes(off:off+1)   = typecast(obj.available_channels    ,'int8'); off=off+2;
+            bytes(off:off+1)   = typecast(obj.active_channels       ,'int8'); off=off+2;
+            bytes(off:off+127) = typecast(obj.channel_mask          ,'int8'); off=off+128;
+            bytes(off:off+1)   = typecast(obj.discard_pre           ,'int8'); off=off+2;
+            bytes(off:off+1)   = typecast(obj.discard_post          ,'int8'); off=off+2;
+            bytes(off:off+1)   = typecast(obj.center_sample         ,'int8'); off=off+2;
+            bytes(off:off+1)   = typecast(obj.encoding_space_ref    ,'int8'); off=off+2;
+            bytes(off:off+1)   = typecast(obj.trajectory_dimensions ,'int8'); off=off+2;
+            bytes(off:off+3)   = typecast(obj.sample_time_us        ,'int8'); off=off+4;
+            bytes(off:off+11)  = typecast(obj.position              ,'int8'); off=off+12;
+            bytes(off:off+11)  = typecast(obj.read_dir              ,'int8'); off=off+12;
+            bytes(off:off+11)  = typecast(obj.phase_dir             ,'int8'); off=off+12;
+            bytes(off:off+11)  = typecast(obj.slice_dir             ,'int8'); off=off+12;
+            bytes(off:off+11)  = typecast(obj.patient_table_position,'int8'); off=off+12;
+            bytes(off:off+33)  = obj.idx.toBytes();                           off=off+34;
+            bytes(off:off+31)  = typecast(obj.user_int              ,'int8'); off=off+32;
+            bytes(off:off+31)  = typecast(obj.user_float            ,'int8');
+        end
+        
+        % The flag methods
+        function val = isFlagSet(obj,flagstring)
+            flagbit = ismrmrd.FlagBit(getfield(ismrmrd.AcquisitionFlags,flagstring));
+            val = flagbit.isSet(obj.flags);
+        end
+        
+        function obj = setFlag(obj,flagstring)
+            flagbit = ismrmrd.FlagBit(getfield(ismrmrd.AcquisitionFlags,flagstring));
+            obj.flags = bitor(obj.flags, flagbit.bitmask);
+        end
+        
+        % Set methods
         function obj = set.version(obj,v)
             obj.version = uint16(v);
         end
-
+        
         function obj = set.flags(obj,v)
             obj.flags = uint64(v);
         end
@@ -82,7 +124,7 @@ classdef AcquisitionHeader
         
         function obj = set.physiology_time_stamp(obj,v)
             if (length(v)~=8)
-                error('AcquisitionHeader.physiology_time_stamp must have 3 elements')
+                error('AcquisitionHeader.physiology_time_stamp must have 8 elements')
             end
             obj.physiology_time_stamp = uint32(v);
         end
@@ -129,7 +171,7 @@ classdef AcquisitionHeader
         function obj = set.sample_time_us(obj,v)
             obj.sample_time_us = single(v);
         end
-
+        
         function obj = set.position(obj,v)
             if (length(v)~=3)
                 error('AcquisitionHeader.position must have 3 elements')
@@ -140,21 +182,21 @@ classdef AcquisitionHeader
         function obj = set.read_dir(obj,v)
             if (length(v)~=3)
                 error('AcquisitionHeader.read_dir must have 3 elements')
-            end            
+            end
             obj.read_dir = single(v);
         end
         
         function obj = set.phase_dir(obj,v)
             if (length(v)~=3)
                 error('AcquisitionHeader.phase_dir must have 3 elements')
-            end            
+            end
             obj.phase_dir = single(v);
         end
         
         function obj = set.slice_dir(obj,v)
             if (length(v)~=3)
                 error('AcquisitionHeader.slice_dir must have 3 elements')
-            end            
+            end
             obj.slice_dir = single(v);
         end
         
@@ -164,12 +206,12 @@ classdef AcquisitionHeader
             end
             obj.patient_table_position = single(v);
         end
-                
+        
         function obj = set.idx(obj,v)
             if isa(v,'ismrmrd.EncodingCounters')
                 obj.idx = v;
             else
-                % not of the correct type, hope it's a struct 
+                % not of the correct type, hope it's a struct
                 % and try to copy one element at a time
                 u = fieldnames(obj.idx);
                 for p = 1:length(u)
--- matlab/+ismrmrd/ImageHeader.m
+++ matlab/+ismrmrd/ImageHeader.m
@@ -1,16 +1,16 @@
 classdef ImageHeader
-
+    
     properties
         version = uint16(0);                          % First unsigned int indicates the version %
-        flags = uint64(0);                             % bit field with flags %
+        flags = uint64(0);                            % bit field with flags %
         measurement_uid = uint32(0);                  % Unique ID for the measurement %
         matrix_size = zeros(3,1,'uint16');            % Pixels in the 3 spatial dimensions
         field_of_view = zeros(3,1,'single');          % Size (in mm) of the 3 spatial dimensions %
         channels = uint16(0);                         % Number of receive channels %
         position = zeros(3,1,'single');               % Three-dimensional spatial offsets from isocenter %
-        read_dir = zeros(3,1,'single');         % Directional cosines of the readout/frequency encoding %
-        phase_dir = zeros(3,1,'single');           % Directional cosines of the phase encoding %
-        slice_dir = zeros(3,1,'single');         % Directional cosines of the slice %
+        read_dir = zeros(3,1,'single');               % Directional cosines of the readout/frequency encoding %
+        phase_dir = zeros(3,1,'single');              % Directional cosines of the phase encoding %
+        slice_dir = zeros(3,1,'single');              % Directional cosines of the slice %
         patient_table_position = zeros(3,1,'single'); % Patient table off-center %
         average = uint16(0);                          % e.g. signal average number %
         slice = uint16(0);                            % e.g. imaging slice number %
@@ -19,7 +19,7 @@ classdef ImageHeader
         repetition = uint16(0);                       % e.g. dynamic number for dynamic scanning %
         set = uint16(0);                              % e.g. flow encodning set %
         acquisition_time_stamp = uint32(0);           % Acquisition clock %
-        physiology_time_stamp = zeros(3,1,'uint32');  % Physiology time stamps, e.g. ecg, breating, etc. %
+        physiology_time_stamp = zeros(8,1,'uint32');  % Physiology time stamps, e.g. ecg, breating, etc. %
         image_data_type = uint16(0);                  % e.g. unsigned short, float, complex float, etc. %
         image_type = uint16(0);                       % e.g. magnitude, phase, complex, real, imag, etc. %
         image_index = uint16(0);					  % e.g. image number in series of images  %
@@ -31,41 +31,83 @@ classdef ImageHeader
     methods
         
         % Constructor
-        function obj = ImageHeader(s)
+        function obj = ImageHeader(bytes)
             if (nargin == 1)
-                obj.version = s.version;
-                obj.flags = s.flags;
-                obj.measurement_uid = s.measurement_uid;
-                obj.matrix_size = s.matrix_size;
-                obj.field_of_view = s.field_of_view;
-                obj.channels = s.channels;
-                obj.position = s.position;
-                obj.read_dir = s.read_dir;
-                obj.phase_dir = s.phase_dir;
-                obj_slice_dir = s.slice_dir;
-                obj.patient_table_position = s.patient_table_position;
-                obj.average = s.average;
-                obj.slice = s.slice;
-                obj.contrast = s.contrast;
-                obj.phase = s.phase;
-                obj.repetition = s.repetition;
-                obj.set = s.set;
-                obj.acquisition_time_stamp = s.acquisition_time_stamp;
-                obj.physiology_time_stamp = s.physiology_time_stamp;
-                obj.image_data_type = s.image_data_type;
-                obj.image_type = s.image_type;
-                obj.image_index = s.image_index;
-                obj.image_series_index = s.image_series_index;
-                obj.user_int = s.user_int;
-                obj.user_float = s.user_float;
+                off = 1;
+                obj.version                = typecast(bytes(off:off+1),  'uint16'); off=off+2;
+                obj.flags                  = typecast(bytes(off:off+7),  'uint64'); off=off+8;
+                obj.measurement_uid        = typecast(bytes(off:off+3),  'uint32'); off=off+4;
+                obj.matrix_size            = typecast(bytes(off:off+5),  'uint16'); off=off+6;
+                obj.field_of_view          = typecast(bytes(off:off+11), 'single'); off=off+12;
+                obj.channels               = typecast(bytes(off:off+1),  'uint16'); off=off+2;
+                obj.position               = typecast(bytes(off:off+11), 'single'); off=off+12;
+                obj.read_dir               = typecast(bytes(off:off+11), 'single'); off=off+12;
+                obj.phase_dir              = typecast(bytes(off:off+11), 'single'); off=off+12;
+                obj.slice_dir              = typecast(bytes(off:off+11), 'single'); off=off+12;
+                obj.patient_table_position = typecast(bytes(off:off+11), 'single'); off=off+12;
+                obj.average                = typecast(bytes(off:off+1),  'uint16'); off=off+2;
+                obj.slice                  = typecast(bytes(off:off+1),  'uint16'); off=off+2;
+                obj.contrast               = typecast(bytes(off:off+1),  'uint16'); off=off+2;
+                obj.phase                  = typecast(bytes(off:off+1),  'uint16'); off=off+2;
+                obj.repetition             = typecast(bytes(off:off+1),  'uint16'); off=off+2;
+                obj.set                    = typecast(bytes(off:off+1),  'uint16'); off=off+2;
+                obj.acquisition_time_stamp = typecast(bytes(off:off+3),  'uint32'); off=off+4;
+                obj.physiology_time_stamp  = typecast(bytes(off:off+31), 'uint32'); off=off+32;
+                obj.image_data_type        = typecast(bytes(off:off+1),  'uint16'); off=off+2;
+                obj.image_type             = typecast(bytes(off:off+1),  'uint16'); off=off+2;
+                obj.image_index            = typecast(bytes(off:off+1),  'uint16'); off=off+2;
+                obj.image_series_index     = typecast(bytes(off:off+1),  'uint16'); off=off+2;
+                obj.user_int               = typecast(bytes(off:off+31), 'uint32'); off=off+32;
+                obj.user_float             = typecast(bytes(off:off+31), 'single');
             end
         end
-
-        % Set methods 
+        
+        function bytes = toBytes(obj)
+            bytes = zeros(214,1,'int8');
+            off = 1;
+            bytes(off:off+1)   = typecast(obj.version               ,'int8'); off=off+2;
+            bytes(off:off+7)   = typecast(obj.flags                 ,'int8'); off=off+8;
+            bytes(off:off+3)   = typecast(obj.measurement_uid       ,'int8'); off=off+4;
+            bytes(off:off+5)   = typecast(obj.matrix_size           ,'int8'); off=off+6;
+            bytes(off:off+11)  = typecast(obj.field_of_view         ,'int8'); off=off+12;
+            bytes(off:off+1)   = typecast(obj.channels              ,'int8'); off=off+2;
+            bytes(off:off+11)  = typecast(obj.position              ,'int8'); off=off+12;
+            bytes(off:off+11)  = typecast(obj.read_dir              ,'int8'); off=off+12;
+            bytes(off:off+11)  = typecast(obj.phase_dir             ,'int8'); off=off+12;
+            bytes(off:off+11)  = typecast(obj.slice_dir             ,'int8'); off=off+12;
+            bytes(off:off+11)  = typecast(obj.patient_table_position,'int8'); off=off+12;
+            bytes(off:off+1)   = typecast(obj.average               ,'int8'); off=off+2;
+            bytes(off:off+1)   = typecast(obj.slice                 ,'int8'); off=off+2;
+            bytes(off:off+1)   = typecast(obj.contrast              ,'int8'); off=off+2;
+            bytes(off:off+1)   = typecast(obj.phase                 ,'int8'); off=off+2;
+            bytes(off:off+1)   = typecast(obj.repetition            ,'int8'); off=off+2;
+            bytes(off:off+1)   = typecast(obj.set                   ,'int8'); off=off+2;
+            bytes(off:off+3)   = typecast(obj.acquisition_time_stamp,'int8'); off=off+4;
+            bytes(off:off+31)  = typecast(obj.physiology_time_stamp ,'int8'); off=off+32;
+            bytes(off:off+1)   = typecast(obj.image_data_type       ,'int8'); off=off+2;
+            bytes(off:off+1)   = typecast(obj.image_type            ,'int8'); off=off+2;
+            bytes(off:off+1)   = typecast(obj.image_index           ,'int8'); off=off+2;
+            bytes(off:off+1)   = typecast(obj.image_series_index    ,'int8'); off=off+2;
+            bytes(off:off+31)  = typecast(obj.user_int              ,'int8'); off=off+32;
+            bytes(off:off+31)  = typecast(obj.user_float            ,'int8');
+        end
+        
+        % The flag methods
+        function val = isFlagSet(obj,flagstring)
+            flagbit = ismrmrd.FlagBit(getfield(ismrmrd.ImageFlags,flagstring));
+            val = flagbit.isSet(obj.flags);
+        end
+        
+        function obj = setFlag(obj,flagstring)
+            flagbit = ismrmrd.FlagBit(getfield(ismrmrd.ImageFlags,flagstring));
+            obj.flags = bitor(obj.flags, flagbit.bitmask);
+        end
+        
+        % Set methods
         function obj = set.version(obj,v)
             obj.version = uint16(v);
         end
-
+        
         function obj = set.flags(obj,v)
             obj.flags = uint64(v);
         end
@@ -80,7 +122,7 @@ classdef ImageHeader
             end
             obj.matrix_size = uint16(v);
         end
-
+        
         function obj = set.field_of_view(obj,v)
             if (length(v)~=3)
                 error('ImageHeader.field_of_view must have 3 elements')
@@ -102,21 +144,21 @@ classdef ImageHeader
         function obj = set.read_dir(obj,v)
             if (length(v)~=3)
                 error('AcquisitionHeader.read_dir must have 3 elements')
-            end            
+            end
             obj.read_dir = single(v);
         end
         
         function obj = set.phase_dir(obj,v)
             if (length(v)~=3)
                 error('AcquisitionHeader.phase_dir must have 3 elements')
-            end            
+            end
             obj.phase_dir = single(v);
         end
         
         function obj = set.slice_dir(obj,v)
             if (length(v)~=3)
                 error('AcquisitionHeader.slice_dir must have 3 elements')
-            end            
+            end
             obj.slice_dir = single(v);
         end
         
@@ -126,7 +168,7 @@ classdef ImageHeader
             end
             obj.patient_table_position = single(v);
         end
-                
+        
         function obj = set.average(obj,v)
             obj.average = uint16(v);
         end
@@ -156,8 +198,8 @@ classdef ImageHeader
         end
         
         function obj = set.physiology_time_stamp(obj,v)
-            if (length(v)~=3)
-                error('AcquisitionHeader.physiology_time_stamp must have 3 elements')
+            if (length(v)~=8)
+                error('AcquisitionHeader.physiology_time_stamp must have 8 elements')
             end
             obj.physiology_time_stamp = uint32(v);
         end
@@ -190,7 +232,7 @@ classdef ImageHeader
             end
             obj.user_float = single(v);
         end
-                
+        
     end
     
 end
