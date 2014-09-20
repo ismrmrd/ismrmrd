9bb1f3bbc439ceb5119a5869af8cc1c0ff9f6898
Ghislain Antony Vaillant <ghisvail@gmail.com>
2014-09-20 23:08:33 +0100

    add support for cpack
    - to build a binary package: make package
    - to build a source package: make package_source

---
+++ cmake/ismrmrd_cpack.cmake
@@ -0,0 +1,28 @@
+################################################################################
+# Find available package generators
+################################################################################
+
+if(UNIX)
+  # DEB
+  find_program(DPKG_PROGRAM dpkg)
+  if(EXISTS ${DPKG_PROGRAM})
+    list(APPEND CPACK_GENERATOR "DEB")
+  endif(EXISTS ${DPKG_PROGRAM})
+  # RPM
+  find_program(RPMBUILD_PROGRAM rpmbuild)
+  if(EXISTS ${RPMBUILD_PROGRAM})
+    list(APPEND CPACK_GENERATOR "RPM")
+  endif(EXISTS ${RPMBUILD_PROGRAM})
+endif(UNIX)
+
+list(APPEND CPACK_SOURCE_GENERATOR "TGZ")
+list(APPEND CPACK_SOURCE_GENERATOR "ZIP")
+list(APPEND CPACK_SOURCE_IGNORE_FILES ";.git;.gitignore;todo.txt;_clang-format;build/")
+
+# set dependencies explictly
+include(InstallRequiredSystemLibraries)
+set(DEBIAN_PACKAGE_DEPENDS "libhdf5-dev")
+set(RPM_PACKAGE_DEPENDS "hdf5-devel")
+
+# where the package metadata are
+set(ISMRMRD_CPACK_CFG_FILE "${PROJECT_BINARY_DIR}/cpack_options.cmake")
