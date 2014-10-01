d0f8077d64353be7426e5f52884960c253aace5c
Hui Xue <hui.xue@nih.gov>
2014-10-01 10:11:29 -0400

    refine the packaging

--- cmake/ismrmrd_cpack.cmake
+++ cmake/ismrmrd_cpack.cmake
@@ -8,11 +8,11 @@ if(UNIX)
   if(EXISTS ${DPKG_PROGRAM})
     list(APPEND CPACK_GENERATOR "DEB")
   endif(EXISTS ${DPKG_PROGRAM})
-  # RPM
-  find_program(RPMBUILD_PROGRAM rpmbuild)
-  if(EXISTS ${RPMBUILD_PROGRAM})
-    list(APPEND CPACK_GENERATOR "RPM")
-  endif(EXISTS ${RPMBUILD_PROGRAM})
+  # TODO: RPM
+  #find_program(RPMBUILD_PROGRAM rpmbuild)
+  #if(EXISTS ${RPMBUILD_PROGRAM})
+  #  list(APPEND CPACK_GENERATOR "RPM")
+  #endif(EXISTS ${RPMBUILD_PROGRAM})
 endif(UNIX)
 
 if(WIN32)
@@ -20,19 +20,14 @@ if(WIN32)
     list(APPEND CPACK_GENERATOR "NSIS")    
 endif(WIN32)
 
-list(APPEND CPACK_SOURCE_GENERATOR "TGZ")
-list(APPEND CPACK_SOURCE_GENERATOR "ZIP")
+# TODO: list(APPEND CPACK_SOURCE_GENERATOR "TGZ")
+# TODO: list(APPEND CPACK_SOURCE_GENERATOR "ZIP")
 list(APPEND CPACK_SOURCE_IGNORE_FILES ";.git;.gitignore;todo.txt;_clang-format;build/")
 
-# set dependencies explictly
+# set dependencies explicitly
 include(InstallRequiredSystemLibraries)
-set(DEBIAN_PACKAGE_DEPENDS "libhdf5-dev")
-set(RPM_PACKAGE_DEPENDS "hdf5-devel")
+set(DEBIAN_PACKAGE_DEPENDS "libhdf5-7, libfftw3-3, libboost-program-options-dev")
+# TODO: set(RPM_PACKAGE_DEPENDS "hdf5-devel")
 
 # where the package metadata are
 set(ISMRMRD_CPACK_CFG_FILE "${PROJECT_BINARY_DIR}/cpack_options.cmake")
-
-# where the package to be installed
-if (NOT WIN32)
-    set(CPACK_PACKAGING_INSTALL_PREFIX ${CMAKE_INSTALL_PREFIX})
-endif (NOT WIN32)
