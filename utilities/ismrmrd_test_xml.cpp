d4d4597de372bad34a3ff0d25821f956a3412041
Michael S. Hansen <michael.hansen@nih.gov>
2014-12-16 10:05:59 -0500

    Fixed bug in ismrmrd_test_xml.cpp and added explicit casts to prints of unsigned long long to prevent compiler warnings

--- utilities/ismrmrd_test_xml.cpp
+++ utilities/ismrmrd_test_xml.cpp
@@ -8,7 +8,7 @@ int main(int argc, char** argv)
 {
 
   if (argc < 2) {
-    std::cout << "Usage:\n\ttest_ismrmrd_xml <ismrmrd_xml_header.xml>\n" << std::endl;
+    std::cout << "Usage:\n\tismrmrd_test_xml <ismrmrd_xml_header.xml>\n" << std::endl;
     return -1;
   }
 
