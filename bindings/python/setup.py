26029863c712a8bfe3505181dc739ab6c3484cac
Joseph Naegele <joseph.naegele@gmail.com>
2014-12-22 11:33:10 -0500

    uploading Python bindings to pypi

--- bindings/python/setup.py
+++ bindings/python/setup.py
@@ -4,20 +4,40 @@ from distutils.extension import Extension
 from Cython.Distutils import build_ext
 import numpy
 
-ismrmrd_home = os.environ['ISMRMRD_HOME']
+ismrmrd_home = os.environ.get('ISMRMRD_HOME', '')
 
 ext = Extension(
     "ismrmrd",
     ["cismrmrd.pxd", "ismrmrd.pyx"],
     include_dirs=[os.path.join(ismrmrd_home, 'include'),
-                  numpy.get_include()],
-    library_dirs=[os.path.join(ismrmrd_home, 'lib')],
+            '/usr/local/include', '/usr/include',
+            numpy.get_include()],
+    library_dirs=[os.path.join(ismrmrd_home, 'lib'),
+            '/usr/local/lib', '/usr/lib'],
     libraries=["ismrmrd"],
     extra_link_args=[],
 )
 
 setup(
-    name="ismrmrd",
+    name='ismrmrd',
+    version='1.2.0',
+    author='ISMRMRD Developers',
+    author_email='ismrmrd@googlegroups.com',
+    description='Python bindings to ISMRMRD',
+    license='Public Domain',
+    keywords='ismrmrd',
+    url='https://ismrmrd.github.io',
+    packages=[],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Science/Research',
+        'License :: Public Domain',
+        'Operating System :: OS Independent',
+        'Programming Language :: Cython',
+        'Topic :: Scientific/Engineering :: Medical Science Apps.'
+    ],
+    requires=['Cython', 'numpy'],
+
     ext_modules=[ext],
     cmdclass={'build_ext':build_ext}
 )
