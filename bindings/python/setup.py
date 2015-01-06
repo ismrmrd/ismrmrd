a3f9de898a641e81cbed45d299a7bdbb025f9105
Souheil Inati <souheil.inati@nih.gov>
2015-01-06 14:40:58 -0500

    Changed setup.py version number

--- bindings/python/setup.py
+++ bindings/python/setup.py
@@ -39,7 +39,7 @@ ext = Extension(
 
 setup(
     name='ismrmrd',
-    version='1.2.0',
+    version='1.2.1',
     author='ISMRMRD Developers',
     author_email='ismrmrd@googlegroups.com',
     description='Python bindings to ISMRMRD',
