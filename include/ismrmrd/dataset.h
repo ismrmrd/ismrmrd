64931b2ab751df1a327903abcc61365cfeba81a7
vadre <vlad.dreyvitser@nih.gov>
2016-08-05 15:27:35 -0400

    Made Waveform class a template. Updated existing xml header and acquisition related methods of Dataset class per TODOs. Updated unit tests for waveform and dataset. Updated main.cpp, generate_cartesian...cpp, read_timing...cpp, and recon...cpp only for compiling - no functional changes there.

--- include/ismrmrd/dataset.h
+++ include/ismrmrd/dataset.h
@@ -15,73 +15,77 @@ namespace ISMRMRD {
 
 //  ISMRMRD Dataset C++ Interface
 class EXPORTISMRMRD Dataset {
-public:
+
+    // TODO: Thread safety.
+
+    public:
     // Constructor and destructor
     Dataset(const std::string& filename, const std::string& groupname,
             bool create_file_if_needed=true, bool read_only=false);
     ~Dataset();
 
     // XML Header
-    // TODO 1. rename writeHeader into writeXmlHeader, and readHeader into readXmlHeader
-    void writeHeader(const std::string &xmlstring);
-    std::string readHeader();
+    void writeXmlHeader(const std::string &xmlstring);
+    std::string readXmlHeader();
+
+    uint32_t getNumberOfEntities(StreamId stream);
 
     // Acquisitions
-    // TODO 2. Since we are planning for multiple stream numbers for acquisitions, the concept of default stream
-    //         number may be difficult to implement. Hence, propose to remove the -1 option, and require a specific
-    //         value instead.
-    template <typename T> void appendAcquisition(const Acquisition<T>& acq, int stream_number=-1);
-    template <typename T> Acquisition<T> readAcquisition(unsigned long index, int stream_number=-1);
-    unsigned long getNumberOfAcquisitions(int stream_number=-1);
+    template <typename T>
+    void appendAcquisition(const Acquisition<T>& acq, StreamId stream = STREAM_NONE);
+    template <typename T>
+    Acquisition<T> readAcquisition(unsigned long index, StreamId stream = STREAM_NONE);
+    unsigned long getNumberOfAcquisitions(StreamId stream = STREAM_NONE);
 
     // Images
-    // TODO 3. Resolve potential stream number ambiguity. Replace the string type variable with stream number.
-    template <typename T> void appendImage(const std::string &var, const Image<T> &im);
-    template <typename T> Image<T> readImage(const std::string &var, unsigned long index);
-    unsigned long getNumberOfImages(const std::string &var);
+    template <typename T> void appendImage(const Image<T> &im, StreamId stream = STREAM_NONE);
+    template <typename T> Image<T> readImage(unsigned long index, StreamId stream = STREAM_NONE);
+    unsigned long getNumberOfImages(StreamId stream = STREAM_NONE);
+
+    // Waveforms
+    template <typename T>
+    void appendWaveform(const Waveform<T> &wf, StreamId stream = STREAM_NONE);
+    template <typename T>
+    Waveform<T> readWaveform(unsigned long index, StreamId stream = STREAM_NONE);
+    unsigned long getNumberOfWaveforms(StreamId stream = STREAM_NONE);
 
     // NDArrays
-    // TODO 4. Resolve potential stream number ambiguity. Should this data also be identifiable
-    //         by a stream number or should this be a special case?
-    template <typename T> void appendNDArray(const std::string &var, const NDArray<T> &arr);
-    template <typename T> NDArray<T> readNDArray(const std::string &var, unsigned long index);
-    unsigned long getNumberOfNDArrays(const std::string &var);
+    template <typename T> void appendNDArray(const NDArray<T> &arr, StreamId stream = STREAM_NONE);
+    template <typename T> NDArray<T> readNDArray(unsigned long index, StreamId stream = STREAM_NONE);
+    unsigned long getNumberOfNDArrays(StreamId stream = STREAM_NONE);
 
-protected:
+    bool getIndexEntry (uint32_t entry_number, IndexEntry& entry);
+
+    private:
+
     bool linkExists(const std::string& path);
     void createGroup(const std::string& path);
-    // TODO 5. Change constructDataPath to accept existing path and an addition string
-    std::string constructDataPath(unsigned int stream_number);
+    std::string constructPath(std::string base, StreamId stream);
+    std::string constructPath(std::string base, std::string addition);
     size_t appendToDataSet(const std::string& path, const DataType& dtype,
             const std::vector<hsize_t>& dims, void* data);
     void readFromDataSet(const std::string& path, const DataType& dtype,
             const std::vector<hsize_t>& entry_dims, size_t index, void* data);
+    void updateIndexTable();
+    void addIndexEntry(uint32_t index, StreamId stream, uint32_t substream = 0);
 
 
     std::string filename_;
     std::string groupname_;
-    std::string xml_header_path_;
-    // TODO 6. Not sure if data_path_ needs to be a member. If it does, then we might need to change it and add
-    //         unique names for paths to acquisitions, waveforms, images, and et cetera
-    std::string data_path_;
+    std::string xml_header_group_path_;
+    std::string xml_header_data_path_;
     std::string index_path_;
 
-    // TODO 7. The word dataset has three meanings in this library: the name of the library, the groupname(s),
-    //         and the meaning in the context of the H5Cpp library which refers to a directory within the data
-    //         hierarchy. Should we rename datasets_ into directories_? Or nodes_?
-    std::map<std::string, DataSet> datasets_;
+    std::map<std::string, DataSet> h5_datasets_;
+    std::vector<IndexEntry> index_table_;
 
     bool read_only_;
     bool file_open_;
-    // TODO 8. dataset_open_ is not used and probably not needed.
-    bool dataset_open_;
+    bool index_table_updated_;
 
     H5File file_;
 };
 
-// TODO 9. Add append, read, and getNumberOf for waveform.
-// TODO 10. Consider simulteneous access to the same file by multiple applications.
-// TODO 11. Consider method "IndexEntry getNextIndexEntry (uint32_t prev_entry);" for playback functionality.
 
 } /* namespace ISMRMRD */
 
