1b32cb7e2aeb920418c2d6757a46bb56b3b16caa
Kelvin Chow <kelvin.chow@gmail.com>
2022-03-15 22:17:36 -0500

    Fix serialization/deserialization of patientWeight_kg, deviceID, (trajectoryDescriptionType) comment, referencedImageSequenceType, and multiple encoding spaces

--- matlab/+ismrmrd/+xml/serialize.m
+++ matlab/+ismrmrd/+xml/serialize.m
@@ -67,6 +67,7 @@ if isfield(header,'measurementInformation')
         for ref = referencedImageSequence(:)
             append_node(docNode,referencedImageSequenceNode,ref,'referencedSOPInstanceUID');
         end
+        measurementInformationNode.appendChild(referencedImageSequenceNode)
     end
 
     docRootNode.appendChild(measurementInformationNode);
@@ -93,6 +94,7 @@ if isfield(header,'acquisitionSystemInformation')
 
     append_optional(docNode,acquisitionSystemInformationNode,acquisitionSystemInformation,'institutionName');
     append_optional(docNode,acquisitionSystemInformationNode,acquisitionSystemInformation,'stationName',@num2str);
+    append_optional(docNode,acquisitionSystemInformationNode,acquisitionSystemInformation,'deviceID',@num2str);
     docRootNode.appendChild(acquisitionSystemInformationNode);
 end
 
@@ -105,7 +107,7 @@ if ~isfield(header,'encoding')
     error('Illegal header: missing encoding section');
 end
 
-for enc = header.encoding(:)
+for enc = header.encoding(:)'
     node = docNode.createElement('encoding');
 
     append_encoding_space(docNode,node,'encodedSpace',enc.encodedSpace);
@@ -126,7 +128,6 @@ for enc = header.encoding(:)
     node.appendChild(n2);
 
     append_node(docNode,node,enc,'trajectory');
-    node.appendChild(n2);
 
     % sometimes the encoding has the fields, but they are empty
     if isfield(enc,'trajectoryDescription')
