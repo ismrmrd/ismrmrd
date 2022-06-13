601f92a5daf46998ab9db2645c636caf83ba5ce2
David C. Hansen <davidchansen@gradientsoftware.net>
2022-06-13 16:27:03 +0200

    Added missing files

---
+++ tests/test_xml_header.cpp
@@ -0,0 +1,39 @@
+#include "ismrmrd/xml.h"
+#include "ismrmrd/version.h"
+#include "embedded_xml.h"
+#include <boost/test/unit_test.hpp>
+#include <sstream>
+
+using namespace ISMRMRD;
+
+BOOST_AUTO_TEST_SUITE(HeaderTest)
+
+BOOST_AUTO_TEST_CASE(test_basic_xml_header)
+{
+
+    IsmrmrdHeader header;
+    deserialize(basic_xml.c_str(),header);
+    std::stringstream stream;
+    serialize(header,stream);
+
+    IsmrmrdHeader header2;
+    deserialize(stream.str().c_str(),header2);
+    BOOST_CHECK_EQUAL(header,header2);
+}
+
+BOOST_AUTO_TEST_CASE(test_extended_xml_header)
+{
+
+    IsmrmrdHeader header;
+    deserialize(extended_xml.c_str(),header);
+    std::stringstream stream;
+    serialize(header,stream);
+
+    IsmrmrdHeader header2;
+    deserialize(stream.str().c_str(),header2);
+    BOOST_CHECK_EQUAL(header,header2);
+
+
+
+}
+BOOST_AUTO_TEST_SUITE_END()
