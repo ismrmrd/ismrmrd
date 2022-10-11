7ae3729fee8fecb14885cc8b0c3f2f6d59eaad54
Michael Hansen <mihansen@microsoft.com>
2022-10-11 22:26:43 +0000

    Fixed windows build issues

--- tests/test_serialization.cpp
+++ tests/test_serialization.cpp
@@ -124,8 +124,8 @@ BOOST_AUTO_TEST_CASE(test_of_protocol_serialization) {
 
     // Fill trajectory
     for (size_t i = 0; i < acq1.getNumberOfTrajElements(); i++) {
-        acq1.getTrajPtr()[i] = i;
-        acq2.getTrajPtr()[i] = 2 * i;
+        acq1.getTrajPtr()[i] = 1.0f * i;
+        acq2.getTrajPtr()[i] = 2.0f * i;
     }
 
     // A waveform
