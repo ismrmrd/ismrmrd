57433ddb32241f8f865e72d97f3ed5c8eeeea1a9
David C. Hansen <davidchansen@gradientsoftware.net>
2022-11-13 12:36:58 +0100

    Added testing for reading and writing of datasets

---
+++ tests/test_dataset.cpp
@@ -0,0 +1,69 @@
+#include "ismrmrd/ismrmrd.h"
+#include "ismrmrd/dataset.h"
+#include "ismrmrd/version.h"
+#include <boost/test/unit_test.hpp>
+#include <boost/filesystem.hpp>
+#include <boost/random.hpp>
+
+using namespace ISMRMRD;
+
+
+static boost::random::mt19937 rng;
+static boost::random::uniform_real_distribution<float> dist = boost::random::uniform_real_distribution<float>();
+
+float create_random_float() {
+    return dist(rng);
+}
+
+BOOST_AUTO_TEST_SUITE(DatasetTest)
+
+
+BOOST_AUTO_TEST_CASE(test_acquisition_header) {
+    
+
+	boost::filesystem::path temp = boost::filesystem::unique_path();
+
+
+
+	Acquisition acq = Acquisition(32, 4, 2);
+
+
+	std::vector<Acquisition> acqs(10, acq);
+    for (int i = 0; i < acqs.size(); i++) {
+        std::generate((float *)acqs[i].data_begin(), (float *)acqs[i].data_end(), create_random_float);
+        std::generate((float *)acqs[i].traj_begin(), (float *)acqs[i].traj_end(), create_random_float);
+	}
+
+    {
+        Dataset dataset = Dataset(temp.string().c_str(), "/test", true);
+        for (int i = 0; i < acqs.size(); i++)
+        dataset.appendAcquisition(acqs[i]);
+    }
+
+    {
+        Dataset dataset = Dataset(temp.string().c_str(), "/test", false);
+        for (int i = 0; i < acqs.size(); i++) {
+            Acquisition acq;
+            dataset.readAcquisition(i,acq);
+
+            const Acquisition &acq_ref = acqs[i];
+
+            
+            BOOST_REQUIRE(acq.getHead() == acq_ref.getHead());
+
+            BOOST_CHECK(std::equal(acq.data_begin(), acq.data_end(),acq_ref.data_begin()));
+            BOOST_CHECK(std::equal(acq.traj_begin(), acq.traj_end(), acq_ref.traj_begin()));
+
+
+        }     
+    }
+
+    boost::filesystem::remove(temp);
+
+
+        
+
+
+}
+
+BOOST_AUTO_TEST_SUITE_END()
