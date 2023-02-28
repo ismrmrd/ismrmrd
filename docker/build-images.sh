b8fafb48eb2b77dba33afdae42a1ee679fa8673b
Michael Hansen <mihansen@microsoft.com>
2023-02-28 22:14:16 +0000

    Adding command line options to build-images.sh

--- docker/build-images.sh
+++ docker/build-images.sh
@@ -5,6 +5,27 @@ set -eo pipefail
 tag="latest"
 image_base_name="ghcr.io/ismrmrd"
 
+# parse command line arguments
+while [[ $# -gt 0 ]]; do
+    key="$1"
+    case $key in
+        --tag)
+            tag="$2"
+            shift
+            shift
+            ;;
+        --image-base-name)
+            image_base_name="$2"
+            shift
+            shift
+            ;;
+        *)
+            echo "Unknown option $key"
+            exit 1
+            ;;
+    esac
+done
+
 # Dockerfile is in the root director one level up from this script
 dockerfile="$(dirname "${BASH_SOURCE[0]}")/../Dockerfile"
 context="$(dirname "${BASH_SOURCE[0]}")/.."
