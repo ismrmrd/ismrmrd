7ff4ca8cbacdb7cbfa7c957b98f0145393601ee6
Michael Hansen <mihansen@microsoft.com>
2023-03-01 01:47:54 +0000

    Bump version, push images on release

--- docker/build-images.sh
+++ docker/build-images.sh
@@ -4,6 +4,7 @@ set -eo pipefail
 
 tag="latest"
 image_base_name="ghcr.io/ismrmrd"
+push=false
 
 # parse command line arguments
 while [[ $# -gt 0 ]]; do
@@ -19,6 +20,10 @@ while [[ $# -gt 0 ]]; do
             shift
             shift
             ;;
+        --push)
+            push=true
+            shift
+            ;;
         *)
             echo "Unknown option $key"
             exit 1
@@ -32,9 +37,17 @@ context="$(dirname "${BASH_SOURCE[0]}")/.."
 
 # Array for targets to build
 targets=( "ismrmrd-test" "ismrmrd" "ismrmrd-stream-recon" "ismrmrd-stream-recon-server" )
+push_targets=( "ismrmrd" "ismrmrd-stream-recon" "ismrmrd-stream-recon-server" )
 
 # Loop over targets
 for target in "${targets[@]}"; do
     echo "Building ${target}..."
     docker build -t "${image_base_name}/${target}:${tag}" -f "$dockerfile" "$context" --target "$target"
+    #If target is in push_targets, and push is true, push it
+    if [[ " ${push_targets[@]} " =~ " ${target} " ]] && [ "$push" = true ]; then
+        echo "Pushing ${target}..."
+        docker push "${image_base_name}/${target}:${tag}"
+        docker build -t "${image_base_name}/${target}:latest" -f "$dockerfile" "$context" --target "$target"
+        docker push "${image_base_name}/${target}:latest"
+    fi
 done
