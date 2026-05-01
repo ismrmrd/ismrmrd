54fcf49eea67c0001a70beecad377db5de707757
Joe Naegele <jnaegele@microsoft.com>
2026-05-01 12:03:36 -0400

    Add macOS conda build

--- conda/build.sh
+++ conda/build.sh
@@ -9,6 +9,7 @@ cmake -G Ninja \
       -D CMAKE_BUILD_TYPE=Release \
       -D CMAKE_CXX_STANDARD=17 \
       -D CMAKE_INSTALL_PREFIX="${PREFIX}" \
+      ${CONDA_BUILD_SYSROOT:+-DCMAKE_OSX_SYSROOT="${CONDA_BUILD_SYSROOT}"} \
       ../
 
 ninja
