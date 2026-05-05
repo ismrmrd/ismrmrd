036cf440753f2d1f8a0a1562345a5ffea4a5c5a1
Joe Naegele <jnaegele@microsoft.com>
2026-05-05 15:03:12 +0000

    PR feedback

--- conda/build.sh
+++ conda/build.sh
@@ -5,11 +5,16 @@ set -euo pipefail
 mkdir -p build-conda
 cd build-conda
 
+cmake_args=()
+if [[ -n "${CONDA_BUILD_SYSROOT:-}" ]]; then
+    cmake_args+=("-DCMAKE_OSX_SYSROOT=${CONDA_BUILD_SYSROOT}")
+fi
+
 cmake -G Ninja \
       -D CMAKE_BUILD_TYPE=Release \
       -D CMAKE_CXX_STANDARD=17 \
       -D CMAKE_INSTALL_PREFIX="${PREFIX}" \
-      ${CONDA_BUILD_SYSROOT:+-DCMAKE_OSX_SYSROOT="${CONDA_BUILD_SYSROOT}"} \
+      "${cmake_args[@]}" \
       ../
 
 ninja
